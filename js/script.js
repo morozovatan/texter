cff3fcbcecf6e57533e9b6c0214e85a1554cdfeb
Tatiana Morozova <morozovatatianka.com>
2022-10-30 19:06:54 +0600

    replacing textarea with a tinymce framework

--- js/script.js
+++ js/script.js
@@ -7,7 +7,6 @@ const App = {
         bodyValue: '',
         notes: [],
         editorIsActive: false,
-
     }
     ),
     methods: {
@@ -17,12 +16,25 @@ const App = {
         closeEditor() {
             this.editorIsActive = false
         },
+        makeBold() {
+            const b = document.createElement('b')
+            //document.getSelection().surroundContents(b)
+            document.getSelection().getRangeAt(0).surroundContents(b)
+
+            console.log(document.getSelection().getRangeAt(0))
+        },
         addNewNote() {
-            if (this.titleValue !== '' && this.bodyValue !== '') {
-                this.notes.push({ title: this.titleValue, body: this.bodyValue, isOpen: false })
-                this.titleValue = ''
-                this.bodyValue = ''
-            }
+            // if (this.titleValue !== '' && this.bodyValue !== '') {
+            //     this.notes.push({ title: this.titleValue, body: this.bodyValue, isOpen: false })
+            //     this.titleValue = ''
+            //     this.bodyValue = ''
+            // }
+
+            this.notes.push({ title: this.titleValue, body: tinymce.get('note-body').getContent() })
+            // const domNotes = document.querySelector('.notes').querySelectorAll('li')
+            // console.log(domNotes)
+            // domNotes[i].querySelector('note-body').insertAdjacentHTML('afterbegin', notes.body[i])
+
         },
         openNote(i) {
             this.notes[i].isOpen = !this.notes[i].isOpen;
